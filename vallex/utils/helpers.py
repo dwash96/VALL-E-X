09c1d58e70771b6453495d9d1cc35925fb5597b7
Dustin Washington <dwash96@gmail.com>
2024-04-18 19:59:16 -0400

    Ope, fix Pathlib parents

--- vallex/utils/helpers.py
+++ vallex/utils/helpers.py
@@ -4,4 +4,4 @@ def with_base_path(relative_path = '', alternate_base = ''):
     if(alternate_base):
         return f"{alternate_base}/{relative_path}";
     else:
-        return f"{str(Path(__file__).parents()[1])}/{relative_path}";
+        return f"{str(Path(__file__).parents[1])}/{relative_path}";
